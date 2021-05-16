41daf3ec80c6534b3b2048bd838d7d4b231ebad4
bey.ss <sergeybey17@gmail.com>
2021-05-17 01:40:08 +0700

    #3 added layout screens

---
+++ WhatCook/EnteringProducts/View/EnteringProductsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EnteringProductsViewController.swift
+//  WhatCook
+//
+//  Created by Сергей  Бей on 16.05.2021.
+//
+
+import UIKit
+import Foundation
+
+class EnteringProductsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var enteringProductsTextField: UITextField!
+    @IBOutlet weak var enteringProductsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WhatCook/Recipes/View/Cells/RecipesViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RecipesViewCell.swift
+//  WhatCook
+//
+//  Created by Сергей  Бей on 16.05.2021.
+//
+
+import UIKit
+
+class RecipesViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var foodDishNameLabel: UILabel!
+    @IBOutlet weak var foodDishImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
